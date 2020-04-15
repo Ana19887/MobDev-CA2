54c0690f96e3098e79e6704f3415eafceb43ac2c
Ana19887 <o.anapaulaalves@gmail.com>
2020-04-15 15:14:48 +0000

    change characters

---
+++ MobDev-CA2/src/app/pages/episodes/episodes.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
+import { HttpClient } from '@angular/common/http';
+
+
+
+@Component({
+  selector: 'app-episodes',
+  templateUrl: './episodes.page.html',
+  styleUrls: ['./episodes.page.scss'],
+})
+
+export class EpisodesPage implements OnInit {
+
+  episodes: Observable<any>;
+
+  constructor(private router: Router, private api: ApiService) { }
+
+  ngOnInit() {
+      this.episodes = this.api.getEpisodes();
+  }
+}
