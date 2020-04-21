b9ba3ce649c93ca94b1eac9f6c1a71bf7516de26
Ana19887 <o.anapaulaalves@gmail.com>
2020-04-21 09:22:43 +0000

    add quote

--- MobDev-CA2/src/app/pages/quotes/quotes.page.ts
+++ MobDev-CA2/src/app/pages/quotes/quotes.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../services/api.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-quotes',
