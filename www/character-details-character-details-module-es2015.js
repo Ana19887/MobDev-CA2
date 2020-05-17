3cabf3d8e6e5b8f13c0b6440dfb6f3caba873d68
Ana19887 <o.anapaulaalves@gmail.com>
2020-05-17 18:34:22 +0000

    heroku

---
+++ www/character-details-character-details-module-es2015.js
@@ -0,0 +1,176 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["character-details-character-details-module"],{
+
+/***/ "./node_modules/raw-loader/dist/cjs.js!./src/app/pages/character-details/character-details.page.html":
+/*!***********************************************************************************************************!*\
+  !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/pages/character-details/character-details.page.html ***!
+  \***********************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("<ion-header>\n  <ion-toolbar color=\"primary\">\n   <ion-buttons slot=\"start\" defaultHref=\"/tabs/character\">\n    <ion-back-button></ion-back-button>\n    </ion-buttons>\n    <ion-title>{{character?.name}}</ion-title>\n    <ion-buttons slot=\"end\">\n    <ion-button (click)=\"unfavouriteCharacter()\" *ngIf=\"isFavouriteC\">\n    <ion-icon name=\"thumbs-up\" slot=\"icon-only\"></ion-icon>\n    </ion-button>\n     <ion-button (click)=\"favouriteCharacter()\" *ngIf=\"!isFavouriteC\">\n      <ion-icon name=\"thumbs-down\" slot=\"icon-only\"></ion-icon>\n    </ion-button>\n  </ion-buttons>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n<ion-card *ngIf=\"character\"  class=\"character-card\">\n    <ion-card-content> \n   <ion-icon name=\"happy\" slot=\"start\"></ion-icon>\n    Nickname: {{character.nickname }}\n  </ion-card-content>\n  <ion-card-content> \n  <ion-icon name=\"calendar\" slot=\"start\"></ion-icon>\n   Birthday: {{character.birthday }}\n  </ion-card-content>\n  <ion-card-content> \n   Occupation: {{character.occupation }}\n  </ion-card-content>\n  </ion-card>\n  </ion-content>");
+
+/***/ }),
+
+/***/ "./src/app/pages/character-details/character-details-routing.module.ts":
+/*!*****************************************************************************!*\
+  !*** ./src/app/pages/character-details/character-details-routing.module.ts ***!
+  \*****************************************************************************/
+/*! exports provided: CharacterDetailsPageRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CharacterDetailsPageRoutingModule", function() { return CharacterDetailsPageRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm2015/router.js");
+/* harmony import */ var _character_details_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./character-details.page */ "./src/app/pages/character-details/character-details.page.ts");
+
+
+
+
+const routes = [
+    {
+        path: '',
+        component: _character_details_page__WEBPACK_IMPORTED_MODULE_3__["CharacterDetailsPage"]
+    }
+];
+let CharacterDetailsPageRoutingModule = class CharacterDetailsPageRoutingModule {
+};
+CharacterDetailsPageRoutingModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]],
+    })
+], CharacterDetailsPageRoutingModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/pages/character-details/character-details.module.ts":
+/*!*********************************************************************!*\
+  !*** ./src/app/pages/character-details/character-details.module.ts ***!
+  \*********************************************************************/
+/*! exports provided: CharacterDetailsPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CharacterDetailsPageModule", function() { return CharacterDetailsPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm2015/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm2015/forms.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/fesm2015/ionic-angular.js");
+/* harmony import */ var _character_details_routing_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./character-details-routing.module */ "./src/app/pages/character-details/character-details-routing.module.ts");
+/* harmony import */ var _character_details_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./character-details.page */ "./src/app/pages/character-details/character-details.page.ts");
+
+
+
+
+
+
+
+let CharacterDetailsPageModule = class CharacterDetailsPageModule {
+};
+CharacterDetailsPageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [
+            _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+            _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["IonicModule"],
+            _character_details_routing_module__WEBPACK_IMPORTED_MODULE_5__["CharacterDetailsPageRoutingModule"]
+        ],
+        declarations: [_character_details_page__WEBPACK_IMPORTED_MODULE_6__["CharacterDetailsPage"]]
+    })
+], CharacterDetailsPageModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/pages/character-details/character-details.page.scss":
+/*!*********************************************************************!*\
+  !*** ./src/app/pages/character-details/character-details.page.scss ***!
+  \*********************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (".character-card {\n  background: #b5f0d9;\n  border: 1px solid var(--ion-color-primary);\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi93b3Jrc3BhY2UvTW9iRGV2LUNBMi9zcmMvYXBwL3BhZ2VzL2NoYXJhY3Rlci1kZXRhaWxzL2NoYXJhY3Rlci1kZXRhaWxzLnBhZ2Uuc2NzcyIsInNyYy9hcHAvcGFnZXMvY2hhcmFjdGVyLWRldGFpbHMvY2hhcmFjdGVyLWRldGFpbHMucGFnZS5zY3NzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0VBQ0ksbUJBQUE7RUFDQSwwQ0FBQTtBQ0NKIiwiZmlsZSI6InNyYy9hcHAvcGFnZXMvY2hhcmFjdGVyLWRldGFpbHMvY2hhcmFjdGVyLWRldGFpbHMucGFnZS5zY3NzIiwic291cmNlc0NvbnRlbnQiOlsiLmNoYXJhY3Rlci1jYXJkIHtcbiAgICBiYWNrZ3JvdW5kOiAjYjVmMGQ5O1xuICAgIGJvcmRlcjogMXB4IHNvbGlkIHZhcigtLWlvbi1jb2xvci1wcmltYXJ5KTtcbn1cbiIsIi5jaGFyYWN0ZXItY2FyZCB7XG4gIGJhY2tncm91bmQ6ICNiNWYwZDk7XG4gIGJvcmRlcjogMXB4IHNvbGlkIHZhcigtLWlvbi1jb2xvci1wcmltYXJ5KTtcbn0iXX0= */");
+
+/***/ }),
+
+/***/ "./src/app/pages/character-details/character-details.page.ts":
+/*!*******************************************************************!*\
+  !*** ./src/app/pages/character-details/character-details.page.ts ***!
+  \*******************************************************************/
+/*! exports provided: CharacterDetailsPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CharacterDetailsPage", function() { return CharacterDetailsPage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm2015/router.js");
+/* harmony import */ var _services_api_service__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../services/api.service */ "./src/app/services/api.service.ts");
+/* harmony import */ var _services_favourite_service__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./../../services/favourite.service */ "./src/app/services/favourite.service.ts");
+
+
+
+
+
+let CharacterDetailsPage = class CharacterDetailsPage {
+    constructor(activatedRoute, api, favouriteService) {
+        this.activatedRoute = activatedRoute;
+        this.api = api;
+        this.favouriteService = favouriteService;
+        this.characterId = null;
+        this.isFavouriteC = false;
+    }
+    ngOnInit() {
+        this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
+        this.api.getCharacter(this.characterId).subscribe(res => {
+            this.character = res[0];
+        });
+        this.favouriteService.isFavourite(this.characterId).then(isFav => {
+            this.isFavouriteC = isFav;
+        });
+    }
+    favouriteCharacter() {
+        this.favouriteService.favouriteCharacter(this.characterId).then(() => {
+            this.isFavouriteC = true;
+        });
+    }
+    unfavouriteCharacter() {
+        this.favouriteService.unfavouriteCharacter(this.characterId).then(() => {
+            this.isFavouriteC = false;
+        });
+    }
+};
+CharacterDetailsPage.ctorParameters = () => [
+    { type: _angular_router__WEBPACK_IMPORTED_MODULE_2__["ActivatedRoute"] },
+    { type: _services_api_service__WEBPACK_IMPORTED_MODULE_3__["ApiService"] },
+    { type: _services_favourite_service__WEBPACK_IMPORTED_MODULE_4__["FavouriteService"] }
+];
+CharacterDetailsPage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+        selector: 'app-character-details',
+        template: tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"](__webpack_require__(/*! raw-loader!./character-details.page.html */ "./node_modules/raw-loader/dist/cjs.js!./src/app/pages/character-details/character-details.page.html")).default,
+        styles: [tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"](__webpack_require__(/*! ./character-details.page.scss */ "./src/app/pages/character-details/character-details.page.scss")).default]
+    }),
+    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_angular_router__WEBPACK_IMPORTED_MODULE_2__["ActivatedRoute"], _services_api_service__WEBPACK_IMPORTED_MODULE_3__["ApiService"],
+        _services_favourite_service__WEBPACK_IMPORTED_MODULE_4__["FavouriteService"]])
+], CharacterDetailsPage);
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=character-details-character-details-module-es2015.js.map
